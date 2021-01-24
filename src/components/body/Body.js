4f0f479ada2a7586bff6e910338210bd87b1b985
nguyenductuananh <nguyentuananh.phuphuc@gmail.com>
2021-01-24 14:02:44 +0700

    Change front end construct and scss too for Body.js/.scss

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,18 +1,22 @@
-import React from 'react';
-
-export function Body(props){
+import React, { Component } from 'react';
+import "../../scss/body.scss";
+import Categories from "./Categories";
+import Item from './Item';
+export default function Body(props){
     return (
-        <div className="body">
-            <Categories/>
-            <div className="body__items">
-                {
-                    this.props.items.map((i) => <Item data={i} />)
-                }
+        <div className="body--bg">
+            <div className="body">
+                <div className="body__left">
+                    <input type='text' placeholder="Search" className="body__left--input"/>
+                    <Categories />
+                </div>
+                <div className="body__items">
+                    <Item />
+                </div>
             </div>
             <div className="body__pagination">
                 
             </div>
         </div>
-
     );
 }
