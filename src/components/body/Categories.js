cee1588147f91d3860b86be8be1cb9d94778f63d
nguyenductuananh <nguyentuananh.phuphuc@gmail.com>
2021-02-02 23:48:14 +0700

    Change structure to help filter by category

--- src/components/body/Categories.js
+++ src/components/body/Categories.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import "../../scss/categories.scss"
+import React, { Component } from "react";
+import "../../scss/categories.scss";
 class Categories extends Component {
-    render() { 
-        return (
-            <div className="categories">
-                <h3 className="categories__title">Categories</h3>
-                <ul className="categories__list">
-                    {
-                        this.props.data.map((c) => <li key={c._id} className="categories__item">{c.name}</li>)
-                    }
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="categories">
+        <h3 className="categories__title">Categories</h3>
+        <ul className="categories__list">
+          {this.props.data.map((c) => (
+            <li
+              key={c._id}
+              onClick={() => this.props.onClickCategory(c.name)}
+              className="categories__item"
+            >
+              {c.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
- 
+
 export default Categories;
--- src/components/body/Item.js
+++ src/components/body/Item.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../../scss/item.scss";
 class Item extends Component {
-    render() { 
-        let item = this.props.data;
-        console.log(item);
-        return (
-            <div className="item">
-                <img alt="cover-pic" className="item__cover-img" src="./img/cover.jpg" />
-                <div>
-                    <h5 className="item__categories">{
-                        item.categories.join(", ")
-                    }</h5>
-                    <h5 className="item__title">{item.title}</h5>
-                    <p className="item__desc">{item.content.slice(0, 200)}</p>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    let item = this.props.data;
+    return (
+      <div className="item">
+        <img
+          alt="cover-pic"
+          className="item__cover-img"
+          src="./img/cover.jpg"
+        />
+        <div>
+          <h5 className="item__categories">{item.categories.join(", ")}</h5>
+          <h5 className="item__title">{item.title}</h5>
+          <p className="item__desc">{item.content.slice(0, 200)}</p>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Item;
